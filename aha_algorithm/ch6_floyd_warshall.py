502ad068b5bbcb0134b37c79549799cd74358fae
gucb <chuan.gu6@gmail.com>
2016-01-31 20:38:45 +0800

    chapter 5 .1 floyd-warshall 多源最短路径

--- aha_algorithm/ch6_floyd_warshall.py
+++ aha_algorithm/ch6_floyd_warshall.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 
-# 深度优先遍历有向图
+# floyd-warshall 多源最短路径
 
-n = 5
+n = 4
 min_distance = 99999999
 book = [0 for x in range(20)]
 
@@ -15,13 +15,13 @@ for i in range(1, n + 1):
 
 # a,b,c 表示a到b城市的里程为c
 data_str = '''1,2,2
-1,5,10
+1,3,6
+1,4,4
 2,3,3
-2,5,7
-3,1,4
-3,4,4
-4,5,5
-5,3,3'''
+3,1,7
+3,4,1
+4,1,5
+4,3,12'''
 
 for line in data_str.split('\n'):
     items = line.split(',')
@@ -29,35 +29,19 @@ for line in data_str.split('\n'):
     dir_graph[i][j] = int(items[2])
     # dir_graph[j][i] = int(items[2]) # 无向图
 
-
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         print '%8d' % (dir_graph[i][j],),
     print
 
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if dir_graph[i][j] > dir_graph[i][k] + dir_graph[k][j]:
+                dir_graph[i][j] = dir_graph[i][k] + dir_graph[k][j]
 
-def dfs(cur, dis):
-    global min_distance
-    if dis > min_distance:
-        return
-
-    if cur == n:
-
-        if dis < min_distance:
-            min_distance = dis
-
-        return
-
-    for j in range(1, n + 1):
-        if dir_graph[cur][j] != 99999999 and book[j] == 0:
-            book[j] = 1
-            dfs(j, dis + dir_graph[cur][j])
-            book[j] = 0
-
-    return
-
-
-book[1] = 1
-dfs(1, 0)
-
-print min_distance
+    print '-' * 50
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            print '%8d' % (dir_graph[i][j],),
+        print
